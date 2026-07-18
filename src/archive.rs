e78f8841acc5df5cd0b83f691954c5889f23e716
Emre Yagci <me@meyagci.com>
2026-07-18 20:53:27 +0100

    move archive create and related functions to its own file

--- src/archive.rs
+++ src/archive.rs
@@ -4,9 +4,11 @@
 //! the sqlite database to raw imports and media files are stored in this folder.
 //! Users can create a new Koli folder, or choose an existing one.
 
+pub mod create;
+pub use create::*;
+
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
-use thiserror::Error;
 use uuid::Uuid;
 
 type Uuidv7 = Uuid;
@@ -29,26 +31,6 @@ struct ManifestFile {
     createdAt: IsoDateTime,
 }
 
-/// Creates a new Koli folder, which has:
-/// - koli.json (likely to be deprecated with a db table later on)
-/// - koli.db
-pub fn create(folder_path: &str) -> Result<(), ArchiveError> {
-    match fs::read_dir(folder_path) {
-        Err(why) => Err(ArchiveError::IoError(why)),
-        Ok(paths) => {
-            // The dir has to be empty
-            if paths.count() == 0 {
-                create_manifest_file(folder_path)?;
-                //TODO: 2. Initiliaze the db
-                //TODO: 3. Done. Consider returning the path or something
-                Ok(())
-            } else {
-                Err(ArchiveError::DirNotEmpty)
-            }
-        }
-    }
-}
-
 pub fn open(folder_path: &str) {
     todo!()
 }
@@ -59,27 +41,6 @@ fn koli_folder_files() -> KoliFolderFiles {
         database_file_name: String::from("koli.db"),
         account_dir_name: String::from("accounts"),
         current_format_version: 1,
-    }
-}
-
-fn create_manifest_file(folder_path: &str) -> Result<(), ArchiveError> {
-    let manifest_content = ManifestFile {
-        r#type: String::from("koli"),
-        formatVersion: 1,
-        id: Uuid::now_v7(),
-        createdAt: Utc::now(),
-    };
-
-    match serde_json::to_string(&manifest_content) {
-        Err(e) => Err(ArchiveError::SerdeError(e)),
-        Ok(x) => {
-            fs::write(
-                format!("{folder_path}{}", koli_folder_files().manifest_file_name),
-                &x,
-            )
-            .unwrap();
-            Ok(())
-        }
     }
 }
 
---
+++ src/archive/create.rs
@@ -0,0 +1,77 @@
+use super::{ManifestFile, koli_folder_files};
+use crate::error::ArchiveError;
+
+use chrono::Utc;
+use sqlx::{Sqlite, SqlitePool, migrate::MigrateDatabase};
+use std::fs;
+use uuid::Uuid;
+
+/// Creates a new Koli folder, which has:
+/// - koli.json (likely to be deprecated with a db table later on)
+/// - koli.db
+pub async fn create(folder_path: &str) -> Result<(), ArchiveError> {
+    if !is_dir_empty(&folder_path)? == false {
+        Err(ArchiveError::DirNotEmpty)
+    } else {
+        create_manifest_file(&folder_path)?;
+        init_db(&folder_path).await?;
+        //TODO: 3. Done. Consider returning the path or something
+        Ok(())
+    }
+}
+
+fn is_dir_empty(folder_path: &str) -> Result<bool, ArchiveError> {
+    match fs::read_dir(folder_path) {
+        Err(e) => Err(ArchiveError::IoError(e)),
+        Ok(paths) => {
+            if paths.count() == 0 {
+                Ok(true)
+            } else {
+                Ok(false)
+            }
+        }
+    }
+}
+
+fn create_manifest_file(folder_path: &str) -> Result<(), ArchiveError> {
+    let manifest_content = ManifestFile {
+        r#type: String::from("koli"),
+        formatVersion: 1,
+        id: Uuid::now_v7(),
+        createdAt: Utc::now(),
+    };
+
+    match serde_json::to_string(&manifest_content) {
+        Err(e) => Err(ArchiveError::SerdeError(e)),
+        Ok(x) => {
+            fs::write(
+                format!("{folder_path}{}", koli_folder_files().manifest_file_name),
+                &x,
+            )
+            .unwrap();
+            Ok(())
+        }
+    }
+}
+
+// TODO: Add migration table, and move the sql file in a proper dir
+async fn init_db(folder_path: &str) -> Result<(), ArchiveError> {
+    let db_url = format!("sqlite://{folder_path}koli.db");
+
+    if !Sqlite::database_exists(&db_url).await.unwrap_or(false) {
+        match Sqlite::create_database(&db_url).await {
+            Ok(x) => {
+                let db = SqlitePool::connect(&db_url).await.unwrap();
+                let contents = include_str!("../0000_gray_the_phantom.sql");
+
+                sqlx::raw_sql(contents).execute(&db).await?;
+                db.close().await;
+
+                Ok(x)
+            }
+            Err(e) => Err(ArchiveError::SqlxError(e)),
+        }
+    } else {
+        Err(ArchiveError::KoliDbAlreadyExists)
+    }
+}
