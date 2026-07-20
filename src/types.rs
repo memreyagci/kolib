7830e334df6c7092b2c7fa66a74f3063beda7f11
Emre Yagci <me@meyagci.com>
2026-07-21 00:57:33 +0100

    add Platform enum, and implementations using strum

--- src/types.rs
+++ src/types.rs
@@ -4,6 +4,10 @@ use uuid::Uuid;
 pub type Uuidv7 = Uuid;
 pub type IsoDateTime = DateTime<Utc>;
 
+/// List of supported platforms. strum crate automatically implements functions necessary to get
+/// enum field from string and vice versa.
+#[derive(Debug, strum::EnumString, strum::AsRefStr)]
+#[strum(ascii_case_insensitive)]
 pub enum Platform {
     Twitter,
 }
