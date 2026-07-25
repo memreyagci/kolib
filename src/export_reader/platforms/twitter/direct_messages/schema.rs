f408d365ab75a22d5fe51ea7382e0377c58cdd30
Emre Yagci <me@meyagci.com>
2026-07-25 01:39:05 +0100

    add schema for direct-message.js file of twitter/x
    - it is created using quicktype: https://quicktype.io/, and modified accordingly.
    - it is able to deserialize fields to different data types that are suitable for their relevant db fields on the fly
    - "reaction_key"s can be converted to emoji using to_string() (read TODO)
    - only the necessary fields are validated
    - setting aliases are possible for a platform changes a field's name in the future

---
+++ src/export_reader/platforms/twitter/direct_messages/schema.rs
@@ -0,0 +1,120 @@
+//! Schema of "direct-message.js" file.
+//! Created using quicktype: https://quicktype.io/
+//! and modifications made as needed.
+
+use serde::{Deserialize, Serialize};
+
+pub type DirectMessages = Vec<DirectMessagesElement>;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct DirectMessagesElement {
+    pub dm_conversation: DmConversation,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct DmConversation {
+    pub conversation_id: String,
+
+    pub messages: Vec<Message>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Message {
+    pub message_create: MessageCreate,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde_with::serde_as]
+#[serde(rename_all = "camelCase")]
+pub struct MessageCreate {
+    pub recipient_id: String,
+
+    pub reactions: Vec<Reaction>,
+
+    pub urls: Vec<Url>,
+
+    pub text: String,
+
+    pub media_urls: Vec<String>,
+
+    pub sender_id: String,
+
+    pub id: String,
+
+    // Because in the file, it appears as a string, but actually is a unix timestamp, and db field is
+    // an integer.
+    #[serde_as(as = "DisplayFromStr")]
+    pub created_at: i64,
+
+    pub edit_history: Option<Vec<EditHistory>>,
+
+    // "edited: Option<bool>" is unnecessary, as its appearance is inconsistent, and edit_history already
+    // gives the info on whether the message is edited and more.
+    #[serde(flatten)]
+    pub extra_stuff: serde_json::Value,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde_with::serde_as]
+#[serde(rename_all = "camelCase")]
+pub struct EditHistory {
+    pub edited_text: String,
+
+    #[serde_as(as = "DisplayFromStr")]
+    pub created_at_sec: i64,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde_with::serde_as]
+#[serde(rename_all = "camelCase")]
+pub struct Reaction {
+    pub sender_id: String,
+
+    pub reaction_key: ReactionKey,
+
+    pub event_id: String,
+
+    #[serde_as(as = "DisplayFromStr")]
+    pub created_at: i64,
+}
+
+// TODO: Consider finding a way to convert keywords to emojis without needing to run to_string() function
+#[derive(Debug, Clone, Serialize, Deserialize, strum::Display)]
+#[serde(rename_all = "snake_case")]
+pub enum ReactionKey {
+    #[strum(to_string = "👍")]
+    Agree,
+
+    #[strum(to_string = "👍")]
+    Disagree,
+
+    // Some "reaction_key"s are represented with "emoji" keyword, so it is not possible to know
+    // which emoji was actually sent.
+    Emoji,
+
+    #[strum(to_string = "😂")]
+    Funny,
+
+    #[strum(to_string = "❤️")]
+    Like,
+
+    #[strum(to_string = "😔")]
+    Sad,
+
+    #[strum(to_string = "😮")]
+    Surprised,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Url {
+    pub expanded: String,
+
+    // "url: String" is the shortened "t.co" link of Twitter/x
+    // "display: String" is the truncated version shown in messages
+    // thus, display is all we need
+    #[serde(flatten)]
+    pub extra_stuff: serde_json::Value,
+}
