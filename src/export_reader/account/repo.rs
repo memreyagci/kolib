b317e2fe271713ae19cf232b36f7cfccc03dbd4a
Emre Yagci <me@meyagci.com>
2026-07-28 00:24:29 +0100

    modify account repo to have archive folder field, and to consume modified models and return the new ones

--- src/export_reader/account/repo.rs
+++ src/export_reader/account/repo.rs
@@ -1,4 +1,6 @@
-use sqlx::{Row, SqlitePool, sqlite::SqliteRow};
+use std::path::PathBuf;
+
+use sqlx::{SqlitePool, sqlite::SqliteRow};
 use uuid::Uuid;
 
 use crate::{error::AccountError, export_reader::account::models::AccountModel, types::Platform};
@@ -6,44 +8,63 @@ use crate::{error::AccountError, export_reader::account::models::AccountModel, t
 #[derive(Debug)]
 pub struct AccountRepository {
     pool: SqlitePool,
+    archive_folder: PathBuf,
 }
 
 impl AccountRepository {
-    pub async fn new(pool: SqlitePool) -> Self {
-        Self { pool }
+    pub(crate) fn new(pool: SqlitePool, archive_folder: PathBuf) -> Self {
+        Self {
+            pool,
+            archive_folder,
+        }
     }
 
-    pub async fn insert(&self, name: &str, platform: Platform) -> Result<Uuid, AccountError> {
-        let account = AccountModel::new(name.to_string(), platform);
+    pub async fn create(
+        &self,
+        name: &str,
+        platform: Platform,
+    ) -> Result<AccountModel, AccountError> {
+        let account = AccountModel::new(Uuid::now_v7(), name.to_string(), platform);
 
-        let result = sqlx::query::<_>("INSERT INTO accounts (id, name, platform) VALUES (?, ?, ?) RETURNING id, name, platform;")
-                .bind(account.id())
-                .bind(account.name())
-                .bind(account.platform().to_string())
-                .fetch_one(&self.pool)
-               .await.unwrap();
-
-        Ok(result.get("id"))
-    }
-
-    pub async fn rename(&self, id: Uuid, new_name: &str) -> Result<(), AccountError> {
-        // TODO: Make errors more verbose, e.g "account with ID: {id} doesn't exist"
-        let _ = sqlx::query::<_>("UPDATE accounts SET name = ? WHERE id = ?")
-            .bind(new_name)
-            .bind(id.to_string())
+        let _ = sqlx::query::<_>("INSERT INTO accounts (id, name, platform) VALUES (?, ?, ?)")
+            .bind(account.id())
+            .bind(account.name())
+            .bind(account.platform().to_string())
             .execute(&self.pool)
             .await
             .unwrap();
 
-        Ok(())
+        Ok(account)
     }
 
-    pub async fn delete(&self, id: Uuid) -> Result<(), AccountError> {
+    /// Takes the Account model (consumes it, thus users won't end up continuing to have the
+    /// pre-rename instance) and returns the renamed one.
+    pub async fn rename(
+        &self,
+        account: AccountModel,
+        new_name: &str,
+    ) -> Result<AccountModel, AccountError> {
+        // TODO: Make errors more verbose, e.g "account with ID: {id} doesn't exist"
+        let _ = sqlx::query::<_>("UPDATE accounts SET name = ? WHERE id = ?")
+            .bind(new_name)
+            .bind(account.id().to_string())
+            .execute(&self.pool)
+            .await
+            .unwrap();
+
+        Ok(AccountModel::new(
+            account.id(),
+            new_name.to_owned(),
+            account.platform().to_owned(),
+        ))
+    }
+
+    pub async fn delete(&self, account: AccountModel) -> Result<(), AccountError> {
         // TODO: Make errors more verbose, e.g "account with ID: {id} doesn't exist"
         // TODO: make sure doing so also deletes all related fields from account_datasets and platform
         // file-related tables
         let _ = sqlx::query::<_>("DELETE FROM accounts WHERE id = ?")
-            .bind(id.to_string())
+            .bind(account.id().to_string())
             .execute(&self.pool)
             .await
             .unwrap();
