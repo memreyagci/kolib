245eff40c76c58e4f73c4a785fc328763a6a5c9e
Emre Yagci <me@meyagci.com>
2026-07-28 17:17:43 +0100

    switch to functional implementation in account module
    the struct and its implementation is removed since it was making the
    codebase hard to scale. now, modules don't rely on each other that much,
    but rather accept what is necessary as params and are stateless

--- src/export_reader/account/repo.rs
+++ src/export_reader/account/repo.rs
@@ -1,96 +1,94 @@
-use std::path::PathBuf;
+use std::str::FromStr;
 
-use sqlx::{SqlitePool, sqlite::SqliteRow};
+use sqlx::{Row, SqlitePool};
 use uuid::Uuid;
 
-use crate::{error::AccountError, export_reader::account::models::AccountModel, types::Platform};
+use crate::{error::AccountError, export_reader::account::models::Account, types::Platform};
 
-#[derive(Debug)]
-pub struct AccountRepository {
-    pool: SqlitePool,
-    archive_folder: PathBuf,
+pub async fn create(
+    pool: &SqlitePool,
+    name: &str,
+    platform: Platform,
+) -> Result<Account, AccountError> {
+    let account = Account::new(Uuid::now_v7(), name.to_string(), platform);
+
+    let _ = sqlx::query::<_>("INSERT INTO accounts (id, name, platform) VALUES (?, ?, ?)")
+        .bind(account.id())
+        .bind(account.name())
+        .bind(account.platform().to_string())
+        .execute(pool)
+        .await
+        .unwrap();
+
+    Ok(account)
 }
 
-impl AccountRepository {
-    pub(crate) fn new(pool: SqlitePool, archive_folder: PathBuf) -> Self {
-        Self {
-            pool,
-            archive_folder,
-        }
+/// Takes the Account model (consumes it, thus users won't end up continuing to have the
+/// pre-rename instance) and returns the renamed one.
+pub async fn rename(
+    pool: &SqlitePool,
+    account: Account,
+    new_name: &str,
+) -> Result<Account, AccountError> {
+    // TODO: Make errors more verbose, e.g "account with ID: {id} doesn't exist"
+    let _ = sqlx::query::<_>("UPDATE accounts SET name = ? WHERE id = ?")
+        .bind(new_name)
+        .bind(account.id().to_string())
+        .execute(pool)
+        .await
+        .unwrap();
+
+    Ok(Account::new(
+        account.id(),
+        new_name.to_owned(),
+        account.platform().to_owned(),
+    ))
+}
+
+pub async fn delete(pool: &SqlitePool, account: Account) -> Result<(), AccountError> {
+    // TODO: make sure doing so also deletes all related fields from account_datasets and platform
+    // file-related tables
+    let _ = sqlx::query::<_>("DELETE FROM accounts WHERE id = ?")
+        .bind(account.id().to_string())
+        .execute(pool)
+        .await
+        .unwrap();
+
+    Ok(())
+}
+
+/// Returns an Account instance by its id, which is the unique identifier.
+pub async fn get_by_id(pool: &SqlitePool, id: Uuid) -> Result<Account, AccountError> {
+    // TODO: Make errors more verbose, e.g "account with ID: {id} doesn't exist"
+    let account = sqlx::query::<_>("SELECT id, name, platform FROM accounts WHERE id = ?;")
+        .bind(id.to_string())
+        .fetch_one(pool)
+        .await
+        .unwrap();
+
+    Ok(Account::new(
+        Uuid::from_str(account.try_get("id").unwrap()).unwrap(),
+        account.try_get("name").unwrap(),
+        Platform::from_str(account.try_get("platform").unwrap()).unwrap(),
+    ))
+}
+
+pub async fn get_all(pool: &SqlitePool) -> Vec<Account> {
+    // TODO: If not exists, it will panic. Handle it accordingly.
+    let rows = sqlx::query::<_>("SELECT * FROM accounts;")
+        .fetch_all(pool)
+        .await
+        .unwrap();
+
+    let mut accounts: Vec<Account> = Vec::new();
+
+    for row in rows {
+        accounts.push(Account::new(
+            Uuid::from_str(row.try_get("id").unwrap()).unwrap(),
+            row.try_get("name").unwrap(),
+            Platform::from_str(row.try_get("platform").unwrap()).unwrap(),
+        ));
     }
 
-    pub async fn create(
-        &self,
-        name: &str,
-        platform: Platform,
-    ) -> Result<AccountModel, AccountError> {
-        let account = AccountModel::new(Uuid::now_v7(), name.to_string(), platform);
-
-        let _ = sqlx::query::<_>("INSERT INTO accounts (id, name, platform) VALUES (?, ?, ?)")
-            .bind(account.id())
-            .bind(account.name())
-            .bind(account.platform().to_string())
-            .execute(&self.pool)
-            .await
-            .unwrap();
-
-        Ok(account)
-    }
-
-    /// Takes the Account model (consumes it, thus users won't end up continuing to have the
-    /// pre-rename instance) and returns the renamed one.
-    pub async fn rename(
-        &self,
-        account: AccountModel,
-        new_name: &str,
-    ) -> Result<AccountModel, AccountError> {
-        // TODO: Make errors more verbose, e.g "account with ID: {id} doesn't exist"
-        let _ = sqlx::query::<_>("UPDATE accounts SET name = ? WHERE id = ?")
-            .bind(new_name)
-            .bind(account.id().to_string())
-            .execute(&self.pool)
-            .await
-            .unwrap();
-
-        Ok(AccountModel::new(
-            account.id(),
-            new_name.to_owned(),
-            account.platform().to_owned(),
-        ))
-    }
-
-    pub async fn delete(&self, account: AccountModel) -> Result<(), AccountError> {
-        // TODO: Make errors more verbose, e.g "account with ID: {id} doesn't exist"
-        // TODO: make sure doing so also deletes all related fields from account_datasets and platform
-        // file-related tables
-        let _ = sqlx::query::<_>("DELETE FROM accounts WHERE id = ?")
-            .bind(account.id().to_string())
-            .execute(&self.pool)
-            .await
-            .unwrap();
-
-        Ok(())
-    }
-
-    /// Returns an Account instance by its id, which is the unique identifier.
-    pub async fn get_by_id(&self, id: Uuid) -> SqliteRow {
-        // TODO: Make errors more verbose, e.g "account with ID: {id} doesn't exist"
-        let account = sqlx::query::<_>("SELECT id, name, platform FROM accounts WHERE id = ?;")
-            .bind(id.to_string())
-            .fetch_one(&self.pool)
-            .await
-            .unwrap();
-
-        account
-    }
-
-    pub async fn get_all(&self) -> Vec<SqliteRow> {
-        // TODO: If not exists, it will panic. Handle it accordingly.
-        let rows = sqlx::query::<_>("SELECT * FROM accounts;")
-            .fetch_all(&self.pool)
-            .await
-            .unwrap();
-
-        rows
-    }
+    accounts
 }
