2d0a7afd14e7c12efb70223f43dfe02489d5b292
Emre Yagci <me@meyagci.com>
2026-07-28 17:04:43 +0100

    rename AccountModel struct to Account

--- src/export_reader/account/models.rs
+++ src/export_reader/account/models.rs
@@ -3,13 +3,13 @@ use uuid::Uuid;
 use crate::types::Platform;
 
 #[derive(Debug)]
-pub struct AccountModel {
+pub struct Account {
     id: Uuid,
     name: String,
     platform: Platform,
 }
 
-impl AccountModel {
+impl Account {
     /// To create an account instance to be passed to the repository, name and platform must be set. uuid is
     /// automatically generated.
     ///
@@ -19,7 +19,7 @@ impl AccountModel {
     /// let account = Account::new().name("@my_old_acc".to_string()).platform(Platform::Twitter);
     /// ```
     pub(crate) fn new(id: Uuid, name: String, platform: Platform) -> Self {
-        AccountModel {
+        Account {
             id: id,
             name: name,
             platform: platform,
