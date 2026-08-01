c27c8995ff8261b321c9b381eee74d326df18e7f
Emre Yagci <me@meyagci.com>
2026-08-01 17:33:06 +0100

    add create_db.sh
    to easily create a database of the latest version to be set as
    DATABASE_URL env, which is required by sqlx macros.

---
+++ create_db.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# A shell script to create a database of the latest version.
+# sqlx macros require an existing database for compile-time
+# checking of SQL queries, and this script can be used to
+# create one easily.
+
+db_file="test.db"
+
+if [ -f $db_file ]; then
+  rm $db_file
+fi
+
+for migration_file in src/migrations/; do
+  sqlite3 $db_file <"$migration_file"
+done
