2878d003d756e1cca0302889e78e03b8c50dc8f3
Emre Yagci <me@meyagci.com>
2026-08-10 00:19:32 +0100

    add init_archive_in_temp_dir test helper func and rename "tmp" to "temp" in other functions for consistency

--- src/test_helpers.rs
+++ src/test_helpers.rs
@@ -4,16 +4,25 @@ use std::{fs::File, path::PathBuf};
 
 use tempfile::TempDir;
 
-pub fn create_empty_dir_in_tmp() -> (TempDir, PathBuf) {
+use crate::archive::{self, model::Archive};
+
+pub fn create_empty_dir_in_temp() -> (TempDir, PathBuf) {
     let temp_dir = tempfile::tempdir().unwrap();
     let temp_dir_path = temp_dir.path().to_path_buf();
 
     (temp_dir, temp_dir_path)
 }
 
-pub fn create_non_empty_dir_in_tmp() -> (TempDir, PathBuf) {
-    let (temp_dir, temp_dir_path) = create_empty_dir_in_tmp();
+pub fn create_non_empty_dir_in_temp() -> (TempDir, PathBuf) {
+    let (temp_dir, temp_dir_path) = create_empty_dir_in_temp();
     File::create(temp_dir_path.join("whatever.txt")).unwrap();
+
+    (temp_dir, temp_dir_path)
+}
+
+pub async fn init_archive_in_temp_dir() -> (TempDir, PathBuf) {
+    let (temp_dir, temp_dir_path) = create_empty_dir_in_temp();
+    archive::create(&temp_dir_path).await.unwrap();
 
     (temp_dir, temp_dir_path)
 }
