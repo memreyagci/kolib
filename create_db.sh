aec3d1c4955488aa9bb83bc31ff94120243e7b6d
Emre Yagci <emre@meyagci.com>
2026-09-20 15:36:31 +0100

    make create_db.sh work as expected regardless of the current dir

--- create_db.sh
+++ create_db.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
+set -eu
+
 # A shell script to create a database of the latest version.
 # sqlx macros require an existing database for compile-time
 # checking of SQL queries, and this script can be used to
 # create one easily.
 
-db_file="test.db"
+repo_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+db_file="$repo_dir/test.db"
+schema_file="$repo_dir/docs/database-schema.sql"
 
-if [ -f $db_file ]; then
-  rm $db_file
-fi
+rm -f "$db_file"
 
-for migration_file in src/migrations/*; do
-  sqlite3 $db_file <"$migration_file"
+for migration_file in "$repo_dir"/src/migrations/*.sql; do
+  sqlite3 "$db_file" <"$migration_file"
 done
